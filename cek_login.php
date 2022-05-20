4cc8bbe7eee65d6a1d5bd8c28d1b35d2af328568
Vr <veridetta@gmail.com>
2022-05-20 16:58:17 +0700

    first commit

---
+++ cek_login.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+error_reporting(1);
+include "config/koneksi.php";
+if ($_POST) {
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+	$login=mysqli_query($connect,"SELECT * FROM user WHERE username='$username' AND password='$password'");
+	$ketemu=mysqli_num_rows($login);
+	$r=mysqli_fetch_array($login);
+	
+	// Apabila username dan password ditemukan
+	if ($ketemu > 0){
+	
+	  $_SESSION['namauser']     = $r['username'];
+	  $_SESSION['passuser']     = $r['password'];
+	  $_SESSION['role']		= $r['role'];
+	  header('location:media.php?module=home');
+	}
+	else{
+	  header('location:index.php?log=2');
+	}
+}
+?>
---
+++ config/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "pkm";
+
+// Koneksi dan memilih database di server
+$connect=mysqli_connect($server,$username,$password,$database);
+?>
